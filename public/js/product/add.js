f5435e40617e6148ba90e5a361c1cc9742ec422f
SuperTapok <noname20041@mail.ru>
2023-12-27 22:09:47 +0700

    fix: :bug: Site logo fix

--- public/js/product/add.js
+++ public/js/product/add.js
@@ -1,3 +1,7 @@
+$(document).ready(function () {
+    modalText.innerHTML = 'Форма некорректно заполнена!';
+});
+
 $("#form").submit( (e) => {
     e.preventDefault();
     var form_data = $("#form").serialize();
@@ -12,5 +16,7 @@ $("#form").submit( (e) => {
 });
 
 modalOk.onclick = function () { 
-    location.reload();
+    if (modalText.innerHTML == 'Товар добавлен!') {
+        location.reload();
+    }
 };
